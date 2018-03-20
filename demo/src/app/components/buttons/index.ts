47eb73f6b5ea7f9c41839d7e93909a2879366f91
divdavem <david-emmanuel.divernois@amadeus.com>
2018-03-20 15:07:25 +0100

    Workaround for "illegal state" issue with transitive dependency on ReactiveFormsModule
    cf https://github.com/angular/angular/issues/22820

--- demo/src/app/components/buttons/index.ts
+++ demo/src/app/components/buttons/index.ts
@@ -1,6 +1,7 @@
 export * from './buttons.component';
 
 import {NgModule} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import {NgbdSharedModule} from '../../shared';
 import {NgbdComponentsSharedModule} from '../shared';
@@ -8,7 +9,10 @@ import {NgbdButtons} from './buttons.component';
 import {DEMO_DIRECTIVES} from './demos';
 
 @NgModule({
-  imports: [NgbdSharedModule, NgbdComponentsSharedModule],
+  // For an unknown (and strange) reason, if we don't include here ReactiveFormsModule
+  // (even though it is already exported from NgbdSharedModule), the build fails with
+  // "Error: Illegal State: external references changed in demo/src/app/components/buttons/index.ngfactory.ts"
+  imports: [NgbdSharedModule, NgbdComponentsSharedModule, ReactiveFormsModule],
   exports: [NgbdButtons],
   declarations: [NgbdButtons, ...DEMO_DIRECTIVES]
 })
