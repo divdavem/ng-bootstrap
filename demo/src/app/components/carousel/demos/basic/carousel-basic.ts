d305b8b09ed1a92e4f1cbb0c5059a708788d7918
divdavem <david-emmanuel.divernois@amadeus.com>
2018-03-20 16:58:59 +0100

    Workaround for a bug in ts_devserver: not using HttpClientModule
    cf https://github.com/angular/angular/issues/22866

--- demo/src/app/components/carousel/demos/basic/carousel-basic.ts
+++ demo/src/app/components/carousel/demos/basic/carousel-basic.ts
@@ -1,17 +1,31 @@
 import {Component, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+
+// The dependency on HttpClientModule has been temporarily removed because
+// it causes an issue with ts_devserver, cf https://github.com/angular/angular/issues/22866
+// import {HttpClient} from '@angular/common/http';
+
 import {map} from 'rxjs/operators';
 
 @Component({selector: 'ngbd-carousel-basic', templateUrl: './carousel-basic.html'})
 export class NgbdCarouselBasic implements OnInit {
-  images: Array<string>;
+  images: Array<string> = this._randomImageUrls([
+    {id: 0},
+    {id: 1},
+    {id: 2},
+    {id: 3},
+    {id: 4},
+    {id: 5},
+    {id: 6}
+  ]);
 
-  constructor(private _http: HttpClient) {}
+  constructor(/* private _http: HttpClient */) {}
 
   ngOnInit() {
+    /*
     this._http.get('https://picsum.photos/list')
         .pipe(map((images: Array<{id: number}>) => this._randomImageUrls(images)))
         .subscribe(images => this.images = images);
+    */
   }
 
   private _randomImageUrls(images: Array<{id: number}>): Array<string> {
--- demo/src/app/components/carousel/demos/config/carousel-config.ts
+++ demo/src/app/components/carousel/demos/config/carousel-config.ts
@@ -1,7 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {NgbCarouselConfig} from 'ng_bootstrap/src';
 import {map} from 'rxjs/operators';
-import {HttpClient} from '@angular/common/http';
+
+// The dependency on HttpClientModule has been temporarily removed because
+// it causes an issue with ts_devserver, cf https://github.com/angular/angular/issues/22866
+// import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'ngbd-carousel-config',
@@ -9,9 +12,17 @@ import {HttpClient} from '@angular/common/http';
   providers: [NgbCarouselConfig]  // add NgbCarouselConfig to the component providers
 })
 export class NgbdCarouselConfig implements OnInit {
-  images: Array<string>;
+  images: Array<string> = this._randomImageUrls([
+    {id: 0},
+    {id: 1},
+    {id: 2},
+    {id: 3},
+    {id: 4},
+    {id: 5},
+    {id: 6}
+  ]);
 
-  constructor(config: NgbCarouselConfig, private _http: HttpClient) {
+  constructor(config: NgbCarouselConfig /*, private _http: HttpClient */) {
     // customize default values of carousels used by this component tree
     config.interval = 10000;
     config.wrap = false;
@@ -19,9 +30,11 @@ export class NgbdCarouselConfig implements OnInit {
   }
 
   ngOnInit() {
+    /*
     this._http.get('https://picsum.photos/list')
         .pipe(map((images: Array<{id: number}>) => this._randomImageUrls(images)))
         .subscribe(images => this.images = images);
+    */
   }
 
   private _randomImageUrls(images: Array<{id: number}>): Array<string> {
